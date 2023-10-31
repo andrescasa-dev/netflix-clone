564de2a5a4ecf3fc9bdec988a57a27942a04aca3
Andres Castellanos <andres.about@gmail.com>
2023-10-31 12:30:06 -0500

    movie page: volume and hide panel functionality

--- src/components/atoms/Slider.js
+++ src/components/atoms/Slider.js
@@ -1,8 +1,23 @@
 import styles from '@/styles/Slider.module.css'
-export default function Slider ({ currentValue, handleInput, handleMouseUp }) {
+import { useEffect, useState } from 'react'
+
+// the slider value state and the state of the parent are separated, i want that the parent state only change when mouseUp, in order to prevent bugs in the video player
+export default function Slider ({ handleMouseUp, syncValue, initValue }) {
+  const [value, setValue] = useState(syncValue ?? initValue)
+
+  useEffect(() => {
+    // sync the stateful variable currentValue to the value of the slider
+    if (syncValue) {
+      setValue(syncValue)
+    }
+  }, [syncValue])
+
+  const handleOnInput = (e) => {
+    setValue(e.target.value)
+  }
   return (
     <div className={styles.container}>
-      <input className={styles.slider} type="range" min="0" max="100" step='0.01' value={currentValue} onInput={handleInput} onMouseUp={handleMouseUp}/>
+      <input className={styles.slider} type="range" min="0" max="100" step='0.01' value={value} onInput={handleOnInput} onMouseUp={handleMouseUp}/>
     </div>
   )
 }
--- src/pages/movies/[id].js
+++ src/pages/movies/[id].js
@@ -9,40 +9,67 @@ import { useEffect, useRef, useState } from 'react'
 import Slider from '@/components/atoms/Slider'
 import { useRouter } from 'next/router'
 
+/* todo
+
+- [] refactor slider
+*/
+
 export default function MoviePage () {
+  let timer
   const playerRef = useRef(null)
+  const panelRef = useRef(null)
   const [isClient, setIsClient] = useState(false)
   const [isPlaying, setIsPlaying] = useState(false)
-  const [currentRange, setCurrentRange] = useState(0) // range 0-100, where 1000 is 100% complete video
+  const [currentVideoTime, setCurrentVideoTime] = useState(0) // range 0-100, where 1000 is 100% complete video
   const router = useRouter()
   const videoId = router.query.id
   const title = decodeURI(router.query.title)
+  const [volume, setVolume] = useState(50)
+
+  const handleSetVolume = (e) => {
+    setVolume(e.target.value / 100)
+  }
+
+  const handleMouseUpProgressBar = (e) => {
+    playerRef.current.seekTo(e.target.value / 100, 'fraction')
+  }
+
+  const showPanel = () => {
+    panelRef.current.classList.remove(styles['opacity-0'])
+  }
+
+  const hidePanel = () => {
+    panelRef.current.classList.add(styles['opacity-0'])
+  }
+
+  const handleOnMove = (e) => {
+    clearTimeout(timer)
+    showPanel()
+    timer = setTimeout(() => {
+      console.log('hidding')
+      hidePanel()
+    }, 6000)
+  }
 
   const handleToggleIsPlaying = (e) => {
-    setIsPlaying((prev) => !prev)
+    console.log('toggle playing')
+    setIsPlaying((prev) => {
+      console.log(prev)
+      return !prev
+    })
   }
 
   const handleReadyVideo = (e) => {
     if (playerRef.current) {
-      console.log(playerRef.current.getDuration())
       setIsPlaying(true)
     }
   }
 
   const handleVideoProgress = ({ playedSeconds }) => {
     const durationSeconds = playerRef.current.getDuration()
-    const result = Math.round(playedSeconds) * 100 / durationSeconds
-    if (isPlaying) setCurrentRange(result)
-  }
+    const currentTime = Math.round(playedSeconds) * 100 / durationSeconds
 
-  const handleInputBar = (e) => {
-    setIsPlaying(false)
-    setCurrentRange(e.target.value)
-  }
-
-  const handleBarMouseUp = (e) => {
-    playerRef.current.seekTo(e.target.value / 100, 'fraction')
-    setIsPlaying(true)
+    if (isPlaying) setCurrentVideoTime(currentTime)
   }
 
   useEffect(() => {
@@ -55,23 +82,15 @@ export default function MoviePage () {
         <title>Movie</title>
       </Head>
       <main className={styles.movie}>
-        {/* https://www.youtube.com/watch?v=P7jA9S-Qbpk */}
-        {/* <iframe
-          className={styles.movie__video}
-          id="ytplayer"
-          type="text/html"
-          width="100%"
-          src={'https://www.youtube.com/embed/P7jA9S-Qbpk?autoplay=1&controls=0&'}
-          frameBorder="0">
-        </iframe> */}
-
         {isClient &&
         <div className={styles.movie__video} >
           <ReactPlayer
             url={`https://www.youtube.com/watch?v=${videoId}`}
+            volume={Number(volume)}
             playing={isPlaying}
             height={'100%'}
             width={'100%'}
+            onPause={() => { showPanel() }}
             onReady={handleReadyVideo}
             onProgress={handleVideoProgress}
             ref={playerRef}
@@ -79,17 +98,27 @@ export default function MoviePage () {
         </div>
         }
 
-        <div className={styles.panel}>
+        <div className={styles.panel} ref={panelRef} onMouseMove={handleOnMove}>
           <div className={styles.panel__header}>
             <Icon url={'/arrow.svg'} alt={'go back'} />
             <Text content={title} type={'relevant'} />
           </div>
+          <div className={styles['panel__video-space']} onClick={handleToggleIsPlaying}></div>
           <div className={styles.panel__menu}>
             <div className={styles.panel__controllers}>
               <PlayButton onClick={handleToggleIsPlaying} />
-              <Slider currentValue={currentRange} handleInput={handleInputBar} handleMouseUp={handleBarMouseUp}/>
+              <Slider
+                syncValue={currentVideoTime}
+                handleMouseUp={handleMouseUpProgressBar}
+              />
             </div>
             <div className={styles['panel__interaction-buttons']}>
+              <div>
+                <Slider
+                  initValue={volume}
+                  handleMouseUp={handleSetVolume}
+                />
+              </div>
               <Icon url={'/like.svg'} alt={'like'} />
               <Icon url={'/like.svg'} alt={'dislike'} isUpsideDown={true}/>
               <Icon url={'/share.svg'} alt={'share'} />
